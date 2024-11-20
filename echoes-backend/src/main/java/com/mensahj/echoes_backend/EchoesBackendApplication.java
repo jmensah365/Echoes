b88baed210d049b293aee43d291ad55294d90d3f
jmensah365 <jmensah365@gmail.com>
2024-11-20 14:51:14 -0500

    creating maven proj

---
+++ echoes-backend/src/main/java/com/mensahj/echoes_backend/EchoesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mensahj.echoes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoesBackendApplication.class, args);
+	}
+
+}
---
+++ echoes-backend/src/test/java/com/mensahj/echoes_backend/EchoesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mensahj.echoes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
